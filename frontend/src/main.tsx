b5043b5d1291fe447186f0ccae080a08720a8d82
이어진 <slimin92@naver.com>
2025-03-27 22:11:38 +0900

    feat: add MyFeed, set Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import Layout from './pages/layout.tsx';
+import { RouterProvider } from 'react-router-dom';
+import router from '@/router/router.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Layout>
-      <App />
-    </Layout>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
