96dfed9ac380f874813c9bea56c8d6edd25b5d84
thzthix <seohas0428@naver.com>
2025-03-28 00:29:26 +0900

    style : formatter적용

--- backend/src/main/java/com/example/demo/repository/user/UserRepository.java
+++ backend/src/main/java/com/example/demo/repository/user/UserRepository.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
   Optional<User> findByEmail(String email);
+
   Optional<User> findById(Integer id);
+
   User save(User user);
 
 }
